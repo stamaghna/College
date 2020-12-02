68afee8c91ac69d810028a00260e9c55ed5aaab3
Tamaghna Sarkar <30871863+stamaghna@users.noreply.github.com>
2020-12-02 20:14:05 +0530

    Error if date predates Sysdate

--- 7th Sem/Lab/IT/Assignment_3/Ticketprice/WEB-INF/classes/Discountprice.java
+++ 7th Sem/Lab/IT/Assignment_3/Ticketprice/WEB-INF/classes/Discountprice.java
@@ -30,38 +30,37 @@ public class Discountprice extends HttpServlet
 		//Setting JDBC
 		String dbClassName = "com.mysql.jdbc.Driver";
    		String url = "jdbc:mysql://127.0.0.1:3306/tommy";
-    	String user = "root";
-    	String password = "12345678";
-        try {
-            Class.forName(dbClassName);
-            Connection con = DriverManager.getConnection(url, user, password);
-
-			out.println("<table><tr>");
-    		out.println("<th>FLIGHT ID</th><th>START CITY</th><th>DEST CITY</th><th>PRICE</th></tr>");
+    		String user = "root";
+    		String password = "12345678";
+        	try {
+           	 Class.forName(dbClassName);
+		 Connection con = DriverManager.getConnection(url, user, password);
+		 out.println("<table><tr>");
+		 out.println("<th>FLIGHT ID</th><th>START CITY</th><th>DEST CITY</th><th>PRICE</th></tr>");
 
 			//SQL STATEMENT
-			Statement stmt = con.createStatement();
-			String sql;
+		Statement stmt = con.createStatement();
+		String sql;
          	sql = "SELECT * FROM DISCOUNT;";
          	ResultSet rs = stmt.executeQuery(sql);
 
-			while(rs.next()){
+		while(rs.next()){
             //Retrieve by column name
 //CREATE TABLE DISCOUNT(FLTID CHAR(3) PRIMARY KEY, STRTCT CHAR(15), DESTCT CHAR(15), PRICE INT, STDT DATE, ENDDT DATE);
-            String flightid = rs.getString("FLTID");
-            String startct = rs.getString("STRTCT");
-			String destct = rs.getString("DESTCT");
-			int price = rs.getInt("PRICE");
-			String startdt = rs.getDate("STDT").toString();
-			String enddt = rs.getDate("ENDDT").toString();
+           	String flightid = rs.getString("FLTID");
+            	String startct = rs.getString("STRTCT");
+		String destct = rs.getString("DESTCT");
+		int price = rs.getInt("PRICE");
+		String startdt = rs.getDate("STDT").toString();
+		String enddt = rs.getDate("ENDDT").toString();
 
-			if(sysdate.compareTo(startdt) >= 0 && sysdate.compareTo(enddt) <= 0){
+		if(sysdate.compareTo(startdt) >= 0 && sysdate.compareTo(enddt) <= 0){
 			out.println("<tr><td>"+flightid+"</td>");
 			out.println("<td>"+startct+"</td>");
 			out.println("<td>"+destct+"</td>");
 			out.println("<td>"+price+"</td></tr>");
          	}}
-        } catch (Exception e) {}
+        	} catch (Exception e) {}
 		
 		out.println("</html");
 		out.close();
